f6d44a4ec6f81c3ed68f341ea2632ba5e4d889e3
redhatxxx <redhatxxx@163.com>
2016-08-17 00:26:31 +0800

    sever

--- app/src/main/java/com/example/administrator/myapplication/sever/MediaPlayerService.java
+++ app/src/main/java/com/example/administrator/myapplication/sever/MediaPlayerService.java
@@ -5,12 +5,44 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class MediaPlayerService extends Service {
+    MyThread msgthread;
     public MediaPlayerService() {
     }
 
     @Override
+    public void onStart(Intent intent,int startId){
+        msgthread = new MyThread();
+        msgthread.start();
+        super.onStart(intent,startId);
+    }
+    @Override
+    public void onDestroy(){
+        msgthread.flag = false;
+        super.onDestroy();
+    }
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    class MyThread extends Thread{
+
+        boolean flag = true;//标志位
+
+        int currentstatus = 0;//当前状态
+        @Override
+        public void run(){
+            while(flag){
+                Intent intent = new Intent("MyApplication");
+                intent.putExtra("myThread",currentstatus);
+                sendBroadcast(intent);
+                currentstatus++;
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
