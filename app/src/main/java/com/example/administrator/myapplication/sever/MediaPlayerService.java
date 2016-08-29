c03900655f44b39ae0e2a859c9d4db8bcfa718be
redhatxxx <redhatxxx@163.com>
2016-08-29 17:12:32 +0800

    服务修改

--- app/src/main/java/com/example/administrator/myapplication/sever/MediaPlayerService.java
+++ app/src/main/java/com/example/administrator/myapplication/sever/MediaPlayerService.java
@@ -1,14 +1,29 @@
 package com.example.administrator.myapplication.sever;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+
+import com.example.administrator.myapplication.MainActivity;
 
 public class MediaPlayerService extends Service {
     MyThread msgthread;
+    MediaPlayer audioPlayer;
     public MediaPlayerService() {
     }
 
+    @Override
+    public  void onCreate(){
+        super.onCreate();
+        //设置广播过滤
+        IntentFilter audioFilter = new IntentFilter();
+
+        audioPlayer = new MediaPlayer();
+    }
     @Override
     public void onStart(Intent intent,int startId){
         msgthread = new MyThread();
@@ -21,10 +36,19 @@ public class MediaPlayerService extends Service {
         super.onDestroy();
     }
     @Override
-    public IBinder onBind(Intent intent) {
+    public IBinder onBind(Intent intent)
+    {
         return null;
     }
 
+    //广播接受
+    class MusicReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
+    }
     class MyThread extends Thread{
 
         boolean flag = true;//标志位
